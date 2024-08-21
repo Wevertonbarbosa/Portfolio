ba5a79d28528808d14fc86cef9ae5af7e73459e2
Wevertonbarbosa <wevertonlbs00@gmail.com>
2024-08-20 21:06:04 -0300

    Tela demonstrativa dos meus trabalhos concluido e com responsividade

--- website/src/app/Pages/jobs/jobs.component.ts
+++ website/src/app/Pages/jobs/jobs.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-jobs',
   templateUrl: './jobs.component.html',
-  styleUrl: './jobs.component.css'
+  styleUrl: './jobs.component.css',
 })
 export class JobsComponent {
+  // currentPage = 1;
+
+  // changePage(pageNumber: number): void {
+  //   this.currentPage = pageNumber;
+  // }
+
+
+  totalPages = 11; // ou o número total de páginas que você tiver
+currentPage = 1;
+
+changePage(pageNumber: number): void {
+  if (pageNumber > 0 && pageNumber <= this.totalPages) {
+    this.currentPage = pageNumber;
+  }
+}
 
 }
