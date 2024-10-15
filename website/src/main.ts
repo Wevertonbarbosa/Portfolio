4f11c504dd2ac9b7b0df5b462260d15cb52fc243
Wevertonbarbosa <wevertonlbs00@gmail.com>
2024-10-15 16:14:22 -0300

    Subindo alterações para o PWA

--- website/src/main.ts
+++ website/src/main.ts
@@ -1,8 +1,21 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true
-})
-  .catch(err => console.error(err));
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    ngZoneEventCoalescing: true,
+  })
+  .catch((err) => console.error(err));
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/ngsw-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((err) => {
+      console.error('Service Worker registration failed:', err);
+    });
+}
